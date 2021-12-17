6e8b60948546efd86b650f1fac83d119bec42b15
Sahia123@gmail.com <sahiatech123@gmail.com>
2021-12-17 19:10:40 +0500

    This is my 1st commit

---
+++ AJAX/Models/MyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AJAX.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public MyDbContext(): base("conn")
+        {
+
+        }
+    }
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
